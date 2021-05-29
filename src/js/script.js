f137a05e9eea39308aa299d27fc9923eed6875c9
Roman-00 <rvasin00@gmail.com>
2021-05-29 18:47:04 +0600

    feat: add a promo section 50%

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const hamburger = document.querySelector('.hamburger'),
+          close = document.querySelector('.menu__close');
+          menu = document.querySelector('.menu');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('menu__active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('menu__active');
+    });
+
+    menu.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if(target.classList.contains(close)) {
+            menu.classList.remove('menu__active');
+        } else {
+            target = target.closest('.menu__block');
+            if (!target) {
+                menu.classList.remove('menu__active');
+            }
+        }
+    });
+
+});
