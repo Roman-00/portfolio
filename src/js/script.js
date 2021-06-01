8e09162492e6912a9c872f887486757634bcd983
Roman-00 <rvasin00@gmail.com>
2021-06-01 08:06:20 +0600

    feat: add a website 80%

--- src/js/script.js
+++ src/js/script.js
@@ -25,4 +25,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    /*
+        * Skills 
+    */
+
+    const counters = document.querySelectorAll('.skills__ratings--counter'),
+        lines = document.querySelectorAll('.skills__ratings--line span');
+
+    counters.forEach((item, i) => {
+        
+        lines[i].style.width = item.innerHTML;
+
+    });
+
 });
